fbf1f1d2c1b4dea4da011b72758aec1293585351
PsychedelicsEyes <eyespsychedelcis@gmail.com>
2023-06-15 10:42:54 +0200

    Fix some bugs

--- src/app.js
+++ src/app.js
@@ -1,44 +1,44 @@
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const cors = require('cors');
-  const helmet = require('helmet');
-  const morgan = require('morgan');
-  const mongoose = require('mongoose')
-  require('dotenv').config();
-  const Blacklist = require('./Models/Blacklist');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+const lusca = require('lusca');
+require('dotenv').config();
+const Blacklist = require('./Models/Blacklist');
 
-  const app = express();
+const app = express();
 
-  const ipFilter = async (req, res, next) => {
-    const clientIp = req.ip;
-    
-    const bannedIp = await Blacklist.findOne({ ip: clientIp });
+const ipFilter = async (req, res, next) => {
+  const clientIp = req.ip;
+  
+  const bannedIp = await Blacklist.findOne({ ip: clientIp });
 
-    if (bannedIp) {
-      res.status(403).json({ message: "L'accès a été refusé." });
-    } else {
-      next();
-    }
-  };
+  if (bannedIp) {
+    res.status(403).json({ message: "L'accès a été refusé." });
+  } else {
+    next();
+  }
+};
 
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  app.use(ipFilter);
-  app.use(cors({
-    origin: process.env.CLIENT,
-    credentials: true
-  }));
+app.use(ipFilter);
+app.use(cors({
+  origin: process.env.CLIENT,
+  credentials: true
+}));
 
-  app.use(helmet.xssFilter());
-  app.use(bodyParser.json());
-  app.use(morgan('combined'));
+app.use(lusca.xssProtection(true));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
 
-  const authRoutes = require('./Routes/auth');
-  const userRoutes = require('./Routes/user');
-  const blacklistRoutes = require('./Routes/blacklist');
+const authRoutes = require('./Routes/auth');
+const userRoutes = require('./Routes/user');
+const blacklistRoutes = require('./Routes/blacklist');
 
-  app.use('/api/auth', authRoutes);
-  app.use('/api/user', userRoutes);
-  app.use('/api/blacklist', blacklistRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/blacklist', blacklistRoutes);
 
-  app.listen(process.env.API_PORT, () => console.log(`Server is running on port ${process.env.API_PORT}`));
+app.listen(process.env.API_PORT, () => console.log(`Server is running on port ${process.env.API_PORT}`));
